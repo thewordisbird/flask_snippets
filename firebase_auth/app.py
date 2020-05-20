cbeb60764ea237cd732793b7b2b3a999cafe7f52
Justin Bird <justin@thewordisbird.dev>
2020-05-20 11:19:26 -0700

    OMG! my ajax request was not a promise, converted to jquery ajax request. IT WORKSgot add -A

--- firebase_auth/app.py
+++ firebase_auth/app.py
@@ -5,18 +5,18 @@ Login and manage users with firebase authentication.
 import os
 import datetime
 import json
-from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template, jsonify, abort
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template, jsonify, abort, make_response
 from werkzeug.security import generate_password_hash
 
 # Flask-WTF/WTForms library
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, EqualTo
+from flask_wtf.csrf import CSRFProtect
 
 # Firebase API
 import firebase_admin
 from firebase_admin import credentials, auth
-
 
 
 # Build web form with flask-wtf
@@ -39,6 +39,8 @@ app = Flask(__name__)
 app.config.from_mapping({
     'SECRET_KEY': 'my_secret'
 })
+
+csrf = CSRFProtect(app)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -69,22 +71,23 @@ def login():
 
 @app.route('/profile', methods=['GET'])
 def access_restricted_content():
-    #print(request.form.data)
-    session_cookie = request.cookies.get('fsession')
-    print(session_cookie)
+    print('In profile')
+    session_cookie = request.cookies.get('firebase')
+    print(f'Profile - Session Cookie Val: {session_cookie}')
     if not session_cookie:
         # Session cookie is unavailable. Force user to login.
-        print('session cookie unavailable')
+        print('Profile - Session cookie unavailable')
         return redirect('/login')
 
     # Verify the session cookie. In this case an additional check is added to detect
     # if the user's Firebase session was revoked, user deleted/disabled, etc.
     try:
         decoded_claims = auth.verify_session_cookie(session_cookie, check_revoked=True)
+        
         return render_template('profile.html')
     except auth.InvalidSessionCookieError:
         # Session cookie is invalid, expired or revoked. Force user to login.
-        print("session cookie invalid")
+        print("Profile - Session cookie invalid")
         return redirect('/login')
 
 @app.route('/test', methods=['GET'])
@@ -93,28 +96,37 @@ def test():
 
 @app.route('/sessionLogin', methods=['POST'])
 def session_login():
-   
-    #print(form.data, form.validate_on_submit())
-    #if form.validate_on_submit():
-    #if request.method == 'POST':
-    #print(form.csrf_token)
-    # Get the ID token sent by the client
+    print('In SessionLogin')
+    
     data = request.get_json()
     id_token = data['idToken']
-    csrf_token = data['csrfToken']
-    # Set session expiration to 5 days.
     expires_in = datetime.timedelta(days=5)
     try:
-        print('in try')
         # Create the session cookie. This will also verify the ID token in the process.
         # The session cookie will have the same claims as the ID token.
         session_cookie = auth.create_session_cookie(id_token, expires_in=expires_in)
-        response = jsonify({'status': 'success'})
-        print(f'respopnse {response}')
-        # Set cookie policy for session cookie.
+        resp = jsonify({'status': 'success'})
         expires = datetime.datetime.now() + expires_in
-        response.set_cookie(
-            'fsession', session_cookie, expires=expires, httponly=True, secure=True)
-        return response
-    except Exception:
-        return abort(401, 'Failed to create a session cookie')
+        resp.set_cookie('firebase', session_cookie, expires=expires, httponly=True, secure=False)
+        print(f'SessionLogin - returning resp: {resp}')
+        return resp
+    except Exception as e:
+        print(f'error: {e.args}')
+        return abort(401, 'SessionLogin - Failed to create a session cookie')
+
+@app.route('/set')
+def set_cookie():
+    resp = make_response(redirect(url_for('endpoint')))
+    resp.set_cookie('name', 'sam')
+    return resp
+
+@app.route('/get')
+def get_cookie():
+    name = request.cookies.get('name')
+    return f'Name is {name}'
+
+@app.route('/endpoint')
+def endpoint():
+    name = request.cookies.get('name')
+    print(f'cookie name: {name}')
+    return render_template('profile.html')
--- firebase_auth/static/scripts/app.js
+++ firebase_auth/static/scripts/app.js
@@ -1,66 +1,43 @@
 (function() {
-    // As httpOnly cookies are to be used, do not persist any state client side.
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
-
-    // Get Elements
+   
     const loginForm = document.getElementById('loginForm');
     const txtEmail = document.getElementById('txtEmail');
     const txtPassWord = document.getElementById('txtPassword');
     const btnLogin = document.getElementById('btnLogin');
-    const btnSignOUt = document.getElementById('btnSignOut');
     const csrfToken = document.getElementById('csrf_token');
-    //const userIdToken = document.getElementById('id_token');
 
     function postIdTokenToSessionLogin(endPoint, idToken, csrfToken) {
         const xhttp = new XMLHttpRequest;
-        let jsonData = {idToken: idToken, csrfToken: csrfToken}
-        xhttp.open("POST", endPoint, idToken, true);
+        let jsonData = {idToken: idToken}
+        xhttp.open("POST", endPoint, idToken);
         xhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+        xhttp.setRequestHeader("X-CSRFToken", csrfToken); // CSRF Protection
         xhttp.send(JSON.stringify(jsonData));
     };
 
     // Add Login Event
-    loginForm.addEventListener('submit', e=> {
-        // Prevent From Submit
-        e.preventDefault()
-
+    btnLogin.addEventListener('click', e => {
         // Get Email and Pass
         const email = txtEmail.value;
         const pass = txtPassword.value;
         const auth = firebase.auth();
+
+        // As httpOnly cookies are to be used, do not persist any state client side.
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
         
         // Sign In
-        const promise = auth.signInWithEmailAndPassword(email, pass).then(({ user }) => {
-            //console.log(user.getIdToken())
-            //console.log(document.getElementById('csrf_token').value)
-
-            
+        auth.signInWithEmailAndPassword(email, pass).then(({ user }) => {
             // Get the user's ID token as it is needed to exchange for a session cookie.
             return user.getIdToken().then(idToken => {
                 // Session login endpoint is queried and the session cookie is set.
-                // CSRF protection should be taken into account.
-                // ...
-                //console.log(idToken);
-                //const csrfToken = document.getElementById('csrf_token').value;
-                //userIdToken.value = idToken;
-                //loginForm.submit()
                 return postIdTokenToSessionLogin('/sessionLogin', idToken, csrfToken.value);
             });
         }).then(() => {
-                // A page redirect would suffice as the persistence is set to NONE.
-                return firebase.auth().signOut();
-            }).then(() => {
-                window.location.assign('/profile');
-            });
+            // A page redirect would suffice as the persistence is set to NONE.
+            return auth.signOut();
+        }).then(() => {
+            window.location.assign('/profile');
         });
-        
-    
-
-    
-
-    // Sign Out 
-    btnSignOUt.addEventListener('click', e=> {
-        firebase.auth().signOut();
     });
 
     // Add Realtime Status Listener
@@ -74,3 +51,4 @@
         }
     });
 }());
+
